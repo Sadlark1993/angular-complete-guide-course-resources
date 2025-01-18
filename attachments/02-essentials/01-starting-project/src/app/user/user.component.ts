3f2e5a1f58895fa59f5f82b766b815fa585be791
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-01-18 09:11:43 -0400

    state managing

--- attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -12,14 +10,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  //when this att. changes, the state of the component also changes, cuz this makes changes in the DOM.
+  //this happens because of zone.js
+  selectedUser = DUMMY_USERS[0];
 
   // getter: its used as a property in the template.
   get imagePath() {
+    this.onSelectUser();
     return 'assets/users/' + this.selectedUser.avatar;
   }
 
   onSelectUser() {
-    console.log('Clicked!');
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
   }
 }
