61ba2209391122e16b92279ed6a6f29a4acffae7
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-01-18 11:09:17 -0400

    going back to old way of input

--- attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -10,16 +10,12 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  // input using signals. We will go back to the old way for didactic purposes.
-  avatar = input.required<string>();
-  name = input.required<string>();
-
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  })
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  };
 
   onSelectUser() {
   }
