8f125cd2b405c46ce0032a150fef531522d6f040
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-01-18 12:22:22 -0400

    emitting data from components

--- attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input, Input, signal } from '@angular/core';
+import { Component, computed, EventEmitter, input, Input, Output, output, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -10,13 +10,17 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
+  //select = output<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   };
 
-  onSelectUser() {
+  onClickUser() {
+    this.select.emit(this.id);
   }
 }
