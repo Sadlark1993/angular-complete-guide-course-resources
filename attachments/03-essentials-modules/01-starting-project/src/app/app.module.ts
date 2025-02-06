da28632834c4cd8a32659a9c2f2522ded9e6725f
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-02-05 21:44:47 -0400

    Converting everything to modules

--- attachments/03-essentials-modules/01-starting-project/src/app/app.module.ts
+++ attachments/03-essentials-modules/01-starting-project/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { CardComponent } from './shared/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent, TaskComponent, NewTaskComponent, CardComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent]
+  imports: [BrowserModule, FormsModule]
 })
 export default class AppModule { }
