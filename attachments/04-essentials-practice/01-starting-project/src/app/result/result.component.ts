026b6622741a000e1d40cad913f177c0ce9047c2
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-02-10 17:28:02 -0400

    Implemented @Input() in result compoenent

--- attachments/04-essentials-practice/01-starting-project/src/app/result/result.component.ts
+++ attachments/04-essentials-practice/01-starting-project/src/app/result/result.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { OutputObj } from '../user-input/user-input.models';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-result',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './result.component.html',
   styleUrl: './result.component.css'
 })
 export class ResultComponent {
-
+  @Input() result?: OutputObj[];
 }
