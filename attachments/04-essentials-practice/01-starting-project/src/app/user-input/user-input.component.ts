a671c8bff2d62f26d6494e99e27b6dfbb6f669f8
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-02-10 15:16:46 -0400

    Input template

--- attachments/04-essentials-practice/01-starting-project/src/app/user-input/user-input.component.ts
+++ attachments/04-essentials-practice/01-starting-project/src/app/user-input/user-input.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+type InputObj = {
+  iInvest: number | null,
+  aInvest: number | null,
+  eReturn: number | null,
+  duration: number | null
+}
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  public data: InputObj = {
+    iInvest: null,
+    aInvest: null,
+    eReturn: null,
+    duration: null
+  };
+
+  onSubmit() {
+    console.log(this.data);
+  }
 
 }
