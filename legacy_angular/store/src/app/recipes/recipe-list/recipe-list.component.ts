b3a302f5110775603447d7f710b75ac64099823f
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-04-22 22:49:45 -0400

    Started new section: modules

--- legacy_angular/store/src/app/recipes/recipe-list/recipe-list.component.ts
+++ legacy_angular/store/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://jamdownfoodie.com/wp-content/uploads/2021/05/IMG_3041-585x585.jpg')
+  ];
 
+  constructor() { }
 }
