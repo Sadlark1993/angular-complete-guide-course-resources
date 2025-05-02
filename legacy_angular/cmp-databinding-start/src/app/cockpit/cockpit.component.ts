7286d6725c6e86f1a0657b2edff0dc02858ad858
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-05-01 21:21:42 -0400

    Learning data flux in legacy angular

---
+++ legacy_angular/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrl: './cockpit.component.css'
+})
+export class CockpitComponent {
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer(serverNameInput: HTMLInputElement) {
+    this.serverCreated.emit({ serverName: serverNameInput.value, serverContent: this.newServerContent });
+  }
+
+  onAddBlueprint(serverNameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({ serverName: serverNameInput.value, serverContent: this.newServerContent });
+  }
+}
