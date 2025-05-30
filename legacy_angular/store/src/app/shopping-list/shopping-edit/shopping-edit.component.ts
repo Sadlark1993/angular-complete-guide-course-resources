40a4ad79db9254a9ace20105c435006060a0a662
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-05-29 23:06:27 -0400

    Adding items to shopping list

--- legacy_angular/store/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ legacy_angular/store/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInputRef?: ElementRef;
+  @ViewChild('amountInput') amountInputRef?: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  constructor() { }
+
+  ngOnInit() {
+    // Initialization logic can go here if needed
+  }
+
+  onAddItem() {
+    const name = this.nameInputRef?.nativeElement.value;
+    const amount = this.amountInputRef?.nativeElement.value;
+
+    if (name && amount) {
+      const newIngredient = new Ingredient(name, parseInt(amount, 10));
+      this.ingredientAdded.emit(newIngredient);
+      this.clearInputs();
+    }
+  }
+
+  private clearInputs() {
+    if (this.nameInputRef && this.amountInputRef) {
+      this.nameInputRef.nativeElement.value = '';
+      this.amountInputRef.nativeElement.value = '';
+    }
+  }
 }
